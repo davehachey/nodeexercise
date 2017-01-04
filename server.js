cc94c462042d274d0aded0dc5bec66cba6753b2e
David Hachey <trenchant1@gmail.com>
2017-01-04 12:27:11 -0400

    homework

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  *
  *	UIT Live Code (Node Exercise)
  *  Let's get started with developing some backend technology with Node.js!
- *	
+ *
  *	We'll get started with creating some simple routes using Express.js, and creating our
  *	first custom module. Let's get started!
  *
@@ -14,27 +14,26 @@
  *
 */
 
-// Our dependencies (modules)
 var http = require('http');
 var express = require('express');
-// @TODO: <Problem 2> you should add the messages module here. 
+var messages = require('./messages')
 
-// initialize the app using express
 var app = express();
 
-// this is our first app route using Express.js. This will route the the root url that
-// you are serving. For example, if you are serving on http://localhost:8080, this url
-// will be located at http://localhost:8080/
 app.get('/', function(req, res) {
-
-	// @TODO: <Problem 2> Update your hard coded text into the correct module object
-	res.write('If you see this, your route is working!'); // write something to standard output
-	res.end();	// end the event
+	res.write('If you see this, your route is working!');
+	res.end();
 });
 
-// @TODO: <Problem 1> you should add your new routes here. 
-// @PROTIP: remember to look at the route I provided above!
+app.get('/about', function(req, res) {
+	res.write(messages.about);
+	res.end();
+});
 
-// Initialize the app to listen on port 8080, and log some text to say so
+app.get('/contact', function(req, res) {
+	res.write(messages.contact);
+	res.end();
+});
+
 app.listen(8080);
 console.log("Listening on port 8080...");
